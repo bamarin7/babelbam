9e897441e68bbd536aa1c80456790a34e9c76251
Brian Marin Silva <marinbrian.as@gmail.com>
2023-04-20 14:10:27 -0500

    Adding caching support through indexedDB

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,19 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
- 
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
+
+(async () => {
+  await fileCache.setItem('color', 'red');
+
+  const color = await fileCache.getItem('color');
+
+  console.log(color);
+})()
+
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
@@ -14,7 +27,7 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
           };
         }
 
@@ -22,13 +35,6 @@ export const unpkgPathPlugin = () => {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
         };
-
-        // else if (args.path === 'tiny-test-pkg') {
-        //   return { 
-        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-        //     namespace: 'a'
-        //   };
-        // }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -38,17 +44,28 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
-              console.log(message);
+              import React, { useState } from 'react';
+              console.log(react);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path);
-        return {
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
