a555653ac910a2a862ab8f537397a388ecc992e7
Brian Marin Silva <marinbrian.as@gmail.com>
2023-05-02 15:34:57 -0500

    Adding Redux with Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import ReactDOM from "react-dom";
 // import CodeCell from "./components/code-cell";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import TextEditor from "./components/text-editor";
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 
 const App = () => {
-  return <div>
-    <TextEditor />
-  </div>;
+  return (
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
+  );
 };
 
 ReactDOM.render(
